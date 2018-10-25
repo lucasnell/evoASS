c2be8785c52df3f97fe828df3c31b38d01bb4dff
Lucas Nell <lucas@lucasnell.com>
2018-10-25 18:14:50 -0500

    Added add_perterbation function

--- R/sim.R
+++ R/sim.R
@@ -237,3 +237,103 @@ print.adaptive_dynamics <- function(x, digits = max(3, getOption("digits") - 3),
 
 }
 
+
+
+
+
+
+
+#' Add a perturbation to an `adaptive_dynamics` object.
+#'
+#' @param ad_obj `adaptive_dynamics` object.
+#' @param new_prop Proportion of new clones.
+#' @param new_trait_means Mean(s) of new clones' trait(s).
+#' @param new_N_mean Mean of new clones' abundances.
+#' @param new_N_sd SD of new clones' abundances.
+#' @param which_traits Traits to change. Defaults to `1:length(new_trait_means)`.
+#' @param new_trait_sigmas SDs of new clones' trait(s).
+#'     Defaults to SD of each trait at the end of the previous run of
+#'     `adaptive_dynamics()`.
+#' @param ... Arguments to pass to `adaptive_dynamics()`.
+#'
+#'
+#' @export
+#'
+add_perterbation <- function(ad_obj, new_prop,
+                             new_trait_means,
+                             new_N_mean, new_N_sd,
+                             which_traits = NULL,
+                             new_trait_sigmas = NULL,
+                             ...) {
+
+    if (!inherits(ad_obj, "adaptive_dynamics")) {
+        stop("ad_obj must be adaptive_dynamics object")
+    }
+    if (is.null(which_traits)) which_traits <- 1:length(new_trait_means)
+    if (length(which_traits) != length(new_trait_means)) {
+        stop("\nwhich_traits, and delta_mus must all be the same length.")
+    }
+
+    if (ncol(ad_obj$data) != 5) {
+        stop("\nad_obj should not be edited before inputting here.")
+    }
+    old_clones <- ad_obj %>%
+        .[["data"]] %>%
+        filter(time == max(time)) %>%
+        spread(trait, value) %>%
+        select(-time, -clone, -N) %>%
+        as.matrix()
+
+    if (is.null(new_trait_sigmas)) {
+        new_trait_sigmas <- map_dbl(which_traits, ~ sd(old_clones[,.x]))
+    } else if (length(which_traits) != length(new_trait_sigmas)) {
+        stop("\nnew_trait_sigmas must be NULL or numeric vector the same ",
+             "length as which_traits.")
+    }
+
+    n_old_clones <- nrow(old_clones)
+    n_new_clones <- round(new_prop * n_old_clones)
+    # if (any(new_trait_means < 0)) stop("new_trait_means cannot be < 0")
+
+    new_clones <- matrix(NA_real_, n_new_clones, ncol(old_clones))
+    for (i in 1:ncol(new_clones)) {
+        if (i %in% which_traits) {
+            j <- which(which_traits == i)
+            # new_clones[,i] <- evoASS:::trunc_rnorm_(n_new_clones,
+            #                                         new_trait_means[j],
+            #                                         new_trait_sigmas[j])
+            new_clones[,i] <- rnorm(n = n_new_clones,
+                                    mean = new_trait_means[j],
+                                    sd = new_trait_sigmas[j])
+        } else {
+            new_clones[,i] <- sample(old_clones[,i], n_new_clones, replace = TRUE)
+        }
+    }
+
+    new_traits <- rbind(old_clones, new_clones)
+    new_Ns <- c(ad_obj %>%
+                    .[["data"]] %>%
+                    filter(time == max(time)) %>%
+                    spread(trait, value) %>%
+                    .[["N"]],
+                evoASS:::trunc_rnorm_(n_new_clones, new_N_mean, new_N_sd))
+
+    new_call <- ad_obj$call
+    new_call$V0 <- quote(new_traits)
+    new_call$N0 <- quote(new_Ns)
+    new_args <- list(...)
+    for (x in names(new_args)) {
+        new_call[[x]] <- new_args[[x]]
+    }
+    new_ad <- eval(new_call)
+
+    new_ad$data <- new_ad %>% .[["data"]] %>%
+        mutate(time = time + max(ad_obj$data$time))
+
+    ad_obj$data <- ad_obj$data %>% filter(time < max(time))
+
+    ad_obj$data <- bind_rows(ad_obj$data, new_ad$data)
+
+    return(ad_obj)
+
+}
