e922c0007537ccfd002a36c621e9a1cee5242986
Lucas Nell <lucas@lucasnell.com>
2020-03-27 10:52:05 -0500

    Updated check_derivs for new equations

--- _check_derivs/simulate_data.R
+++ _check_derivs/simulate_data.R
@@ -9,6 +9,7 @@
 # =================================================================
 
 library(readr)
+library(sauron)
 
 q <- 3          # number of traits
 n <- 4          # number of species
@@ -19,13 +20,14 @@ names_ <- c(paste0("V", 1:(n*q)), paste0("N", 1:n), "f", "a0", "eta", "r0", "d")
 
 sim_data <- function(i) { # `i` is just used so the fxn can be passed to lapply
 
-    V <- rnorm(n*q, 0, 5)
     N <- rlnorm(n, log(10), 0.5)
-    f <- rlnorm(1, log(0.2), 0.5)
-    a0 <- rlnorm(1, log(0.1), 0.5)
-    eta <- rnorm(1, 0, 0.1)
-    r0 <- rlnorm(1, log(0.25), 0.25)
-    d <- rnorm(1, 0, 0.2)
+    f <- runif(1, 0, 0.5)
+    a0 <- runif(1, 0, 0.5)
+    eta <- runif(1, -0.5, 0.5)
+    r0 <- runif(1, f * (1 + eta), 2)
+    d <- runif(1, -0.1, 0.1)
+    .sd <- max(as.numeric(unlist(stable_points(eta, f, a0, r0))))
+    V <- abs(rnorm(n*q, 0, .sd))
 
     out <- as.data.frame(rbind(c(V, N, f, a0, eta, r0, d)))
     colnames(out) <- names_
@@ -33,7 +35,7 @@ sim_data <- function(i) { # `i` is just used so the fxn can be passed to lapply
     return(out)
 }
 
-set.seed(78667)
+set.seed(673349653)
 sims <- do.call(rbind, lapply(1:nsims, sim_data))
 
 write_csv(sims, "_check_derivs/simulated_data.csv")
