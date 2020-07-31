36b93bb92afb168c956abb59dd192bd8ab1eef9b
Lucas Nell <lucas@lucasnell.com>
2020-07-31 14:30:14 -0500

    Finished invasion plot

--- _results/first-draft.R
+++ _results/first-draft.R
@@ -27,9 +27,9 @@ cc <- function(.x) {
 }
 
 # whether to re-do simulations (use rds files otherwise)
-.REDO_SIMS <- TRUE
+.REDO_SIMS <- FALSE
 # whether to re-save plots
-.RESAVE_PLOTS <- FALSE
+.RESAVE_PLOTS <- TRUE
 # number of threads to use for simulations
 .N_THREADS <- 3
 
@@ -696,79 +696,79 @@ if (.RESAVE_PLOTS) save_plot(coexist_spp_p, 6.5, 4, .prefix = "2-")
 
 
 
-# ------------------------------------*
-# __ vary d1 & d2; V0 ~ U ----
-# ------------------------------------*
-
-
-
+#' # ------------------------------------*
+#' # __ vary d1 & d2; V0 ~ U ----
+#' # ------------------------------------*
 #'
-#' NOTE: this function always changes both `d` values
 #'
-one_coexist_U_combo <- function(.d, .eta, .add_var, .pb = NULL) {
-
-    if (!is.null(.pb)) .pb$tick(0)
-
-    # .d = 0.15; .eta = etas[[2]]; .add_var = 0.01
-    # rm(.d, .eta, .add_var)
-
-    stopifnot(is.numeric(.d) && length(.d) == 1)
-
-    .d <- rep(.d, 2)
-    .n <- 100
-
-    .V0 <- matrix(runif(.n * 2, 0, 3), nrow = .n)
-
-    .seed <- sample.int(2^31 - 1, 1)
-    set.seed(.seed)
-
-    Z <- quant_gen(q = 2, eta = .eta, d = .d, n = .n,
-                   V0 = .V0, sigma_V0 = 0,
-                   add_var = rep(.add_var, .n),
-                   spp_gap_t = 500L, final_t = 50e3L, n_reps = 12,
-                   save_every = 0L,
-                   n_threads = .N_THREADS, show_progress = FALSE)
-
-    Z$call[["eta"]] <- eval(.eta)
-    Z$call[["d"]] <- eval(.d)
-    Z$call[["n"]] <- eval(.n)
-    Z$call[["add_var"]] <- eval(rep(.add_var, .n))
-
-    jacs <- jacobians(Z)
-
-    if (!is.null(.pb)) .pb$tick()
-
-    return(list(NV = Z$nv %>%
-                    mutate(trait = paste0("V", trait)) %>%
-                    spread(trait, geno) %>%
-                    mutate(d = .d[1], eta = .eta, add_var = .add_var),
-                J = jacs,
-                V0 = .V0,
-                seed = .seed))
-}
-
-
-# Simulations varying both d values
-if (.REDO_SIMS) {
-    # Takes ~XX min w/ 3 threads
-    t0 <- Sys.time()
-    coexist_U_sims <- crossing(.d = seq(-0.25, 2, length.out = 10),
-                             .eta = c(-1,1) * etas[[2]],
-                             .add_var = seq(0.01, 0.1, 0.01)) %>%
-        # bc `seq` makes weird numbers that are ~1e-15 from what they should be:
-        mutate(across(.fns = round, digits = 2))
-    pb <- progress_bar$new(format = " [:bar] :percent in :elapsed | eta: :eta",
-                           total = nrow(coexist_U_sims), clear = FALSE,
-                           show_after = 0)
-    set.seed(2014151565)
-    coexist_U_sims <- coexist_U_sims %>%
-        pmap(one_coexist_U_combo, .pb = pb)
-    saveRDS(coexist_U_sims, rds("coexist_U_sims"))
-    t1 <- Sys.time()
-    t1 - t0; rm(t0, t1)
-} else {
-    coexist_U_sims <- readRDS(rds("coexist_U_sims"))
-}
+#'
+#' #'
+#' #' NOTE: this function always changes both `d` values
+#' #'
+#' one_coexist_U_combo <- function(.d, .eta, .add_var, .pb = NULL) {
+#'
+#'     if (!is.null(.pb)) .pb$tick(0)
+#'
+#'     # .d = 0.15; .eta = etas[[2]]; .add_var = 0.01
+#'     # rm(.d, .eta, .add_var)
+#'
+#'     stopifnot(is.numeric(.d) && length(.d) == 1)
+#'
+#'     .d <- rep(.d, 2)
+#'     .n <- 100
+#'
+#'     .V0 <- matrix(runif(.n * 2, 0, 3), nrow = .n)
+#'
+#'     .seed <- sample.int(2^31 - 1, 1)
+#'     set.seed(.seed)
+#'
+#'     Z <- quant_gen(q = 2, eta = .eta, d = .d, n = .n,
+#'                    V0 = .V0, sigma_V0 = 0,
+#'                    add_var = rep(.add_var, .n),
+#'                    spp_gap_t = 500L, final_t = 50e3L, n_reps = 12,
+#'                    save_every = 0L,
+#'                    n_threads = .N_THREADS, show_progress = FALSE)
+#'
+#'     Z$call[["eta"]] <- eval(.eta)
+#'     Z$call[["d"]] <- eval(.d)
+#'     Z$call[["n"]] <- eval(.n)
+#'     Z$call[["add_var"]] <- eval(rep(.add_var, .n))
+#'
+#'     jacs <- jacobians(Z)
+#'
+#'     if (!is.null(.pb)) .pb$tick()
+#'
+#'     return(list(NV = Z$nv %>%
+#'                     mutate(trait = paste0("V", trait)) %>%
+#'                     spread(trait, geno) %>%
+#'                     mutate(d = .d[1], eta = .eta, add_var = .add_var),
+#'                 J = jacs,
+#'                 V0 = .V0,
+#'                 seed = .seed))
+#' }
+#'
+#'
+#' # Simulations varying both d values
+#' if (.REDO_SIMS) {
+#'     # Takes ~XX min w/ 3 threads
+#'     t0 <- Sys.time()
+#'     coexist_U_sims <- crossing(.d = seq(-0.25, 2, length.out = 10),
+#'                              .eta = c(-1,1) * etas[[2]],
+#'                              .add_var = seq(0.01, 0.1, 0.01)) %>%
+#'         # bc `seq` makes weird numbers that are ~1e-15 from what they should be:
+#'         mutate(across(.fns = round, digits = 2))
+#'     pb <- progress_bar$new(format = " [:bar] :percent in :elapsed | eta: :eta",
+#'                            total = nrow(coexist_U_sims), clear = FALSE,
+#'                            show_after = 0)
+#'     set.seed(2014151565)
+#'     coexist_U_sims <- coexist_U_sims %>%
+#'         pmap(one_coexist_U_combo, .pb = pb)
+#'     saveRDS(coexist_U_sims, rds("coexist_U_sims"))
+#'     t1 <- Sys.time()
+#'     t1 - t0; rm(t0, t1)
+#' } else {
+#'     coexist_U_sims <- readRDS(rds("coexist_U_sims"))
+#' }
 
 
 
@@ -799,7 +799,6 @@ if (.REDO_SIMS) {
 #'
 
 
-.x <- tibble(.d = 0.01, .n = 10, .inv_N0 = 0.1, .eta_sign = 1)
 
 
 one_d_invasion <- function(.x) {
@@ -849,6 +848,7 @@ one_d_invasion <- function(.x) {
                 `>=`(1) %>%
                 as.integer()
             tibble(dist = sqrt((.v1 - v1h)^2 + (.v2 - v2h)^2),
+                   V1 = .v1, V2 = .v2,
                    invaded = .inv)
         }) %>%
         mutate(d = .d, res_n = .n, inv_N0 = .inv_N0, eta = .eta)
@@ -892,6 +892,7 @@ if (.REDO_SIMS) {
 #                      abundance.
 #                      The point color indicates the number of species present
 #                      in the resident community."
+
 
 
 invasion_p_fun <- function(.eta) {
@@ -943,34 +944,12 @@ invasion_p_fun <- function(.eta) {
 invasion_ps <- map(c(-0.6, 0.6), invasion_p_fun)
 names(invasion_ps) <- c("sub", "super")
 
-invasion_ps[["sub"]]
-invasion_ps[["super"]]
-
-# LEFT OFF ----
-# ADD IN STARTING POSITIONS:
-
-v1h <- stable_points(.eta)[["V1"]][1]
-v2h <- stable_points(.eta)[["V2"]][1]
-
-inv_df <- map_dfr(1:8 * 0.1,
-                  function(.dist) {
-                      rbind(c(v1h, v2h + .dist),
-                            c(v1h, v2h - .dist),
-                            c(v1h + .dist, v2h),
-                            c(v1h - .dist, v2h),
-                            c(v1h + .dist / sqrt(2), v2h + .dist / sqrt(2)),
-                            c(v1h - .dist / sqrt(2), v2h - .dist / sqrt(2)),
-                            c(v1h + .dist / sqrt(2), v2h - .dist / sqrt(2)),
-                            c(v1h - .dist / sqrt(2), v2h + .dist / sqrt(2))) %>%
-                          {colnames(.) <- c(".v1", ".v2"); .} %>%
-                          as_tibble()
-                  }) %>%
-    add_row(.v1 = v1h, .v2 = v2h)
 
 
-
-
-# if (.RESAVE_PLOTS) save_plot(invasion_p, 5, 4.5, .prefix = "S2-")
+if (.RESAVE_PLOTS) {
+    save_plot(invasion_ps[["sub"]], 6, 5, .name = "S2-invasion_sub")
+    save_plot(invasion_ps[["super"]], 6, 5, .name = "S2-invasion_super")
+}
 
 
 
