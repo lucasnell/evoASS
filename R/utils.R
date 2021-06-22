fa02ecf23c2c3f803880754f3eaf440838b34d5c
Lucas Nell <lucas@lucasnell.com>
2021-06-22 13:09:35 -0500

    Removed many unused methods/fxns, updated docs

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,4 @@
 
-#' Generic method to perturb an object.
-#'
-#' Implemented only for `adapt_dyn` objects thus far.
-#'
-#' @param obj The object to perturb.
-#' @param ... Additional arguments.
-#' @export
-#'
-perturb <- function(obj, ...) UseMethod("perturb")
-
-
-
 
 #' Black theme to use for presentations
 #'
